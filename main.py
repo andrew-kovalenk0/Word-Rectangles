fc7af353e87ad84ffe5c205e5e506e72a03ecb1d
After2Dark <andrew.kovalenko.2001@mail.ru>
2021-09-23 12:22:41 +0300

    K-means clustering by lines

--- main.py
+++ main.py
@@ -10,32 +10,25 @@ if __name__ == '__main__':
     image = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
     result = cv2.resize(result, dim, interpolation=cv2.INTER_AREA)
 
-    image[image > 140] = 255
+    image[image > 133] = 255
     image = cv2.bitwise_not(image)
     image = cv2.dilate(image, np.ones((3, 3)), iterations=1)
     image = cv2.adaptiveThreshold(image, 255.0, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 3, 20)
 
     counters = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     counters = counters[0] if len(counters) == 2 else counters[1]
-    centers = []
+    lines = []
     for counter in counters:
         M = cv2.moments(counter)
-        cX = int(M["m10"] / M["m00"])
         cY = int(M["m01"] / M["m00"])
-        centers.append([cX, cY])
+        lines.append([cY])
         x, y, w, h = cv2.boundingRect(counter)
         cv2.rectangle(result, (x, y), (x + w, y + h), (0, 0, 255), 1)
 
-    model = KMeans(n_clusters=7)
-    model.fit(centers)
+    model = KMeans(n_clusters=9, init='random', algorithm='full')
+    model.fit(lines)
     for i in model.cluster_centers_:
-        cv2.circle(result, (int(i[0]), int(i[1])), 2, (255, 0, 0), -1)
-    for counter in counters:
-        M = cv2.moments(counter)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        centers.append([cX, cY])
-        cv2.circle(result, (cX, cY), 2, (0, 255, 0), -1)
+        cv2.circle(result, (5, int(i)), 3, (255, 0, 0), -1)
 
     cv2.imshow('result', result)
     cv2.waitKey(0)
